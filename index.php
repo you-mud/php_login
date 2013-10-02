9c7311ce3a7034ac605e9901d6a2561551d3b0fa
you-mud <mud-sheep@live.jp>
2013-10-02 13:46:02 +0900

    regist
    commit

--- index.php
+++ index.php
@@ -58,25 +58,17 @@ if ($_POST['code'] === 'one_code') {
 }else{
 	true;
 }
-
 ?>
+
 <html>
 <body><center><br>
 <form action="" method="POST">
 <?php echo $error."<BR>";?>
 　アドレス<input type="text" name="mail"><br><br>
 パスワード<input type="text" name="pass"><br><br>
-<input type="hidden" name="code" value="one_code">
+<input type="hidden" name="transmit" value="transmission ">
 <input type="submit" value="送信" name="in">
 </form>
 </center>
-
-
-
-
-
-
-
-
 </body>
 </html>
--- regist.php
+++ regist.php
@@ -14,13 +14,36 @@ try{
 	print('Error:'.$e->getMessage());
 	die('error');
 }
+$name = htmlspecialchars($_POST['name']);
+$mail = htmlspecialchars($_POST['mail']);
+$pass = htmlspecialchars($_POST['pass']);
 
+//データベース登録
+if($_POST['regist'] == 'reg'){
+	if($_POST['name'] ==""){
+		$error['name'] = "お名前をご記入くださいませ。<BR>";
+	}else if($_POST['mail'] ==""){
+		$error['name'] = "アドレスをご記入くださいませ。<BR>";
+	}else if($_POST['pass'] ==""){
+		$error['pass'] = "パスワードをご記入くださいませ。<BR>";
+	}else{
+	    true;
+	}
+	$sth = $PDO->prepare('INSERT INTO login (name,mail,pass) VALUES (?,?,?)');
+	$sth->bindValue(1,$name);
+	$sth->bindValue(2,$mail);
+	$sth->bindValue(3,$pass);
+	$sth->execute();
+}
 ?>
 
 <html>
 <body>
 <center>
 <form action="" method="POST">
+<?php if(isset($error)){ ?>
+<?php foreach($error as $ms){echo $ms;}?>
+<?php }?>
 name：<input type="test" name="name"><BR><BR>
 mail：<input type="text" name="mail"><BR><BR>
 pass：<input type="text" name="pass"><BR><BR>
