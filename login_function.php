df86c4d25a45a0da4e578e9ce90b2472ee343b0b
you-mud <mud-sheep@live.jp>
2013-10-02 14:46:17 +0900

    login function
    login

--- login_function.php
+++ login_function.php
@@ -9,3 +9,32 @@ function is_mail($text) {
 		return FALSE;
 	}
 }
+//------------------------
+//半角英数字か確認
+//------------------------
+function is_alnum($text) {
+	if (preg_match("/^[a-zA-Z0-9]+$/",$text)) {
+		return TRUE;
+	} else {
+		return FALSE;
+	}
+}
+//------------------------
+//6文字以上か確認
+//------------------------
+function is_almu($text){
+	if (mb_strlen($text) > 5){
+		true;
+	}else{
+		false;
+	}
+}
+//------------------------
+//
+//------------------------
+
+
+
+
+
+
---
+++ tes.php
@@ -0,0 +1,11 @@
+<?php
+require_once './login_function.php';
+
+
+$one = "222222222222";
+
+if(is_almu($one)){
+	echo "oke";
+}else{
+	echo "dame";
+}
